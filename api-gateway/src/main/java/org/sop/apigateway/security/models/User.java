2dba6715c1aab289c8b681d10982f72db7215c45
RedDawnMaster <soufianeocelot@gmail.com>
2024-05-02 17:43:11 +0100

    usr

--- api-gateway/src/main/java/org/sop/apigateway/security/models/User.java
+++ api-gateway/src/main/java/org/sop/apigateway/security/models/User.java
@@ -10,7 +10,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "users")
+@Table(name = "userst")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

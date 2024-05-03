cf84161cfd8c951ad3657e82aeca758500f66119
RedDawnMaster <soufianeocelot@gmail.com>
2024-05-03 22:33:02 +0100

    User service +

---
+++ user-service/src/test/java/org/sop/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sop.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

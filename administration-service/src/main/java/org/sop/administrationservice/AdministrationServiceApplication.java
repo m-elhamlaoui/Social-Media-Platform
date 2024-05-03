67f7e2ff8a0645e7b1b61a7c6840ecc81bc0fddb
SfaygaTheKing <amoura.ach@gmail.com>
2024-05-03 17:09:52 +0100

    Administration service

---
+++ administration-service/src/main/java/org/sop/administrationservice/AdministrationServiceApplication.java
@@ -0,0 +1,13 @@
+package org.sop.administrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministrationServiceApplication.class, args);
+	}
+
+}
---
+++ administration-service/src/test/java/org/sop/administrationservice/AdministrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sop.administrationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

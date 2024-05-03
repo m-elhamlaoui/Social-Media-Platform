ad98f3a3764792aa64f956e006c9529499a71339
HALIMA DAOUDI <115372645+DAOUDIHalima@users.noreply.github.com>
2024-05-03 23:28:10 +0100

    Post service

--- post-service/src/main/java/org/sop/postservice/model/Comment.java
+++ post-service/src/main/java/org/sop/postservice/model/Comment.java
@@ -4,9 +4,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
-
-
-
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Date;
@@ -14,16 +11,16 @@ import java.util.List;
 import java.util.Objects;
 
 @Entity
+@Table(name = "comments")
 @Getter
 @Setter
-@Table(name = "comments")
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Comment {
-   @Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id; 
+    private Long id;
 
     @Column(length = 1024)
     private String content;
@@ -34,34 +31,13 @@ public class Comment {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private Date dateLastModified;
-    @OneToOne
-    @JoinColumn(name = "author_id", nullable = false)
-    private User author;
+
+   
 
     @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "post_id", nullable = false)
     private Post post;
 
-    @JsonIgnore
-    @ManyToMany
-    @JoinTable(
-            name = "comment_likes",
-            joinColumns = @JoinColumn(name = "comment_id"),
-            inverseJoinColumns = @JoinColumn(name = "liker_id")
-    )
-    private List<User> likeList = new ArrayList<>();
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Comment comment = (Comment) o;
-        return Objects.equals(id, comment.id) && Objects.equals(author, comment.author);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, author);
-    }
+    
 }

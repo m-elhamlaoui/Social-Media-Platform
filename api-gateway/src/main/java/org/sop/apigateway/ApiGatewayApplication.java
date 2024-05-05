f130c8c1ed1f47a1b9dd7c33b83952b74795b561
SfaygaTheKing <amoura.ach@gmail.com>
2024-05-05 15:45:41 +0100

    Docker files for admin servie + api gateway

--- api-gateway/src/main/java/org/sop/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/org/sop/apigateway/ApiGatewayApplication.java
@@ -1,8 +1,15 @@
 package org.sop.apigateway;
 
+import org.sop.apigateway.security.models.ERole;
+import org.sop.apigateway.security.models.Role;
+import org.sop.apigateway.security.repositories.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -12,4 +19,16 @@ public class ApiGatewayApplication {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(RoleRepository roleRepository) {
+        return args -> {
+            List<Role> roles = roleRepository.findAll();
+            if (roles.isEmpty()) {
+                Role roleAdmin = new Role(ERole.ROLE_ADMIN);
+                Role roleUser = new Role(ERole.ROLE_USER);
+                roleRepository.save(roleAdmin);
+                roleRepository.save(roleUser);
+            }
+        };
+    }
 }
